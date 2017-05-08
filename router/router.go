e91a200eccab840b635f80c4d204764a1988205d
Elton Minetto <eminetto@gmail.com>
2017-05-08 13:54:22 -0300

    Resolver problema de cors

--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/coderockr/APIVagaFrontend/controllers"
 
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Initialize(r *gin.Engine) {
@@ -17,18 +18,30 @@ func Initialize(r *gin.Engine) {
 		api.POST("/friends", controllers.CreateFriend)
 		api.PUT("/friends/:id", controllers.UpdateFriend)
 		api.DELETE("/friends/:id", controllers.DeleteFriend)
+		api.OPTIONS("/friends", cors)
+		api.OPTIONS("/friends/:id", cors)
 
 		api.GET("/statuses", controllers.GetStatuses)
 		api.GET("/statuses/:id", controllers.GetStatus)
 		api.POST("/statuses", controllers.CreateStatus)
 		api.PUT("/statuses/:id", controllers.UpdateStatus)
 		api.DELETE("/statuses/:id", controllers.DeleteStatus)
+		api.OPTIONS("/statuses", cors)
+		api.OPTIONS("/statuses/:id", cors)
 
 		api.GET("/users", controllers.GetUsers)
 		api.GET("/users/:id", controllers.GetUser)
 		api.POST("/users", controllers.CreateUser)
 		api.PUT("/users/:id", controllers.UpdateUser)
 		api.DELETE("/users/:id", controllers.DeleteUser)
+		api.OPTIONS("/users", cors)
+		api.OPTIONS("/users/:id", cors)
 
 	}
+}
+
+func cors(c *gin.Context) {
+    c.Header("Access-Control-Allow-Origin", "*")
+    c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
+    c.JSON(http.StatusOK, struct{}{})
 }
